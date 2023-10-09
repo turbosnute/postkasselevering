a23c559c96c1b16cb67ba535d7f4dc21ed7d4282
Øyvind Andreas Nilsen <on@ntnu.no>
2023-10-09 13:07:38 +0200

    this might work better

--- postkasselevering.ps1
+++ postkasselevering.ps1
@@ -1,5 +1,5 @@
 param(
-    $postnr,
+    $postal_code,
     $outfile
 )
 <#
@@ -53,75 +53,37 @@ function New-ICSevent {
     }
 }
 
+#
+# Find API-key
+#
+$postal_code = "7010"
 
-$res = Invoke-WebRequest -Uri "https://www.posten.no/levering-av-post/_/component/main/1/leftRegion/1?postCode=$postnr" `
--Headers @{
-"User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-  "x-requested-with"="XMLHttpRequest"
-  "Accept"="*/*"
-  "Sec-Fetch-Site"="same-origin"
-  "Sec-Fetch-Mode"="cors"
-  "Sec-Fetch-Dest"="empty"
-  "Referer"="https://www.posten.no/levering-av-post-2020"
-  "Accept-Encoding"="gzip, deflate, br"
-  "Accept-Language"="en-NO,en;q=0.9,nb-NO;q=0.8,nb;q=0.7,en-US;q=0.6,no;q=0.5"
-  "dnt"="1"
-  "sec-gpc"="1"
-} `
--ContentType "application/json"
+$url = "https://www.posten.no/levering-av-post"
+$regex_pattern = 'data-react4xp-ref="parts_mailbox-delivery__\S*"\s*type="application\/json">([^<]+)<\/script>'
+$rx=[regex]$regex_pattern
+$result = Invoke-WebRequest -Uri $url
+$apiKey = ($rx.Match($result.RawContent).Groups[1].Value | ConvertFrom-Json).props.apikey
 
-$ics = if ($res.StatusCode -eq 200) {
-
-    # RegEx
-    [regex]$regex = " (\d{1,2})\. ([a-z]+)$"
-
-    # Months
-    $month = @{
-        januar = 1
-        februar = 2
-        marsj = 3
-        april = 4
-        mai = 5
-        juni = 6
-        juli = 7
-        august = 8
-        september = 9
-        oktober = 10
-        november = 11
-        desember = 12
-    }
-
-    $lastday = Get-Date
-    $year = $lastday.Year
-
-    $deliverydays = ($res.Content | ConvertFrom-Json).nextDeliveryDays
-
-    if ($deliverydays.count -ne 0) {             
-           Write-Output "BEGIN:VCALENDAR"
-           Write-Output "VERSION:2.0"
-           Write-Output "METHOD:PUBLISH"
-           Write-Output "PRODID:-//TurboSnute//Postkasselevering//EN"
-           foreach ($stringDate in $deliverydays) {
-             $match = $regex.Match($stringDate)
-             if (($match.Groups[1].success) -and ($match.Groups[2].success)) {
-               $deliveryday = $match.Groups[1].Value
-               $deliveryMonth = $month[$match.Groups[2].Value]
-               #write-host -foregroundcolor green $deliveryday
-               #write-host -foregroundcolor red $($lastday.month)
-               # Check if last day of year.
-               if ($deliveryMonth -lt $lastday.Month) {
-                   $year++
-               }
-               $deliveryDate = Get-Date "$year-$deliveryMonth-$deliveryday 11:00:00"
-               #"$year-$deliveryMonth-$deliveryday"
-               $lastday = $deliveryDate
-               $start = $deliveryDate
-               $end = $deliveryDate.AddHours(3)
-               New-ICSevent -Location $postnr -Subject "Postlevering for $postCode" -Description "Postbudet lever post i dag" -Start $start -End $end -Visibility 'Public' -ShowAs 'Free'
-             } #end if
-           } #end foreach
-           Write-Output "END:VCALENDAR"
-    } #end if
+#
+# Get Delivery Days
+#
+$headers = @{
+    "kp-api-token"="$apiKey"
+    "referer"="https://www.posten.no/levering-av-post"
 }
 
-$ics -replace "`n", "`r`n" | out-file -path $outfile
+$deliveryDays = Invoke-RestMethod -Uri "https://www.posten.no/levering-av-post/_/service/no.posten.website/delivery-days?postalCode=$postal_code" -Headers $headers
+$deliveryDates = $deliveryDays.delivery_dates
+
+$ics = if ($deliveryDates.count -gt 0) {
+    Write-Output "BEGIN:VCALENDAR"
+    Write-Output "VERSION:2.0"
+    Write-Output "METHOD:PUBLISH"
+    Write-Output "PRODID:-//TurboSnute//Postkasselevering//EN"
+
+    foreach ($date in $deliveryDates) {
+        $post_date = (Get-Date $date).AddHours(11)
+        New-ICSevent -Location $postal_code -Subject "Postlevering for $postal_code" -Description "Postbudet lever post i dag" -Start $post_date -End $post_date.AddHours(1) -Visibility 'Public' -ShowAs 'Free'
+    }
+    $ics -replace "`n", "`r`n" | out-file -path $outfile
+}
